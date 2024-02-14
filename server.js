2a07ce1e2ed9917a49ab53c146251b53aea24b15
akhilk49 <akhilkkurian49@gmail.com>
2024-02-14 14:39:56 +0530

    Error handling added in server.js

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/ping', (req, res) => {
     res.send('Hello World');
-  });
+});
 
 app.get('/test', (req, res) => {
     res.send('Hello World testing');
-  });
+});
+
+app.use((req, res) => {
+    res.status(404).send("404 - Route not found");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
