802776d91aba27624b1b543ffac3329e3ad641d6
akhilk49 <akhilkkurian49@gmail.com>
2024-02-16 13:38:17 +0530

    Added docs.bruno folder

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ app.get('/test', (req, res) => {
     res.send('Hello World testing');
 });
 
-app.use((req, res) => {
-    res.status(404).send("404 - Route not found");
-});
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
