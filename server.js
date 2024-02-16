f21df48611b64015df4cd29c492a38e348920552
akhilk49 <akhilkkurian49@gmail.com>
2024-02-16 13:47:25 +0530

    made changes wrt to PR review

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const port = 3000;
 app.get('/ping', (req, res) => {
     res.send('Hello World');
 });
+
 app.get('', (req, res) => {
     res.send('Hi Everyone!...This is my Home page');
 });
 
 app.get('/test', (req, res) => {
     res.send('Hello World testing');
+});
+
+app.use((req, res) => {
+    res.status(404).send("404 - Not Found");
 });
 
 app.listen(port, () => {
